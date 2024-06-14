52dc08d17589ca030835806cedbc8760c9217fca
syayid <muwahhidsyayid@gmail.com>
2024-06-14 20:43:03 +0700

    (+) add management user

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,3 +1,6 @@
+//define url root apps
+const baseL = $("#baseL").val();
+
 $(document).ready(function () {
     //select 2 initially
     $(".select2").select2({
@@ -5,13 +8,53 @@ $(document).ready(function () {
     });
 
     //data table initially
-    let tb = new DataTable(".dataTable", {
+    setDataTable(".dataTable");
+
+    // new Quill(".editor", {
+    //     debug: "info",
+    //     modules: {
+    //         toolbar: true,
+    //     },
+    //     placeholder: "Compose an epic...",
+    //     theme: "snow",
+    // });
+
+    // Swal.fire("SweetAlert2 is working!");
+});
+
+function notif(type, title, message) {
+    Swal.fire({
+        icon: type,
+        title: title,
+        text: message,
+    });
+}
+
+function confirm(title, text, callback = () => {}) {
+    Swal.fire({
+        title,
+        text,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callback();
+        }
+    });
+}
+
+function setDataTable(id) {
+    let tb = new DataTable(id, {
         layout: {
             topStart: null,
             topEnd: null,
             bottomStart: "pageLength",
             bottomEnd: "paging",
         },
+        destroy: true,
     });
 
     $(".dataTable-search").on("keyup", function () {
@@ -29,34 +72,71 @@ $(document).ready(function () {
 
         tb.column(column).search(value).draw();
     });
-
-    // new Quill(".editor", {
-    //     debug: "info",
-    //     modules: {
-    //         toolbar: true,
-    //     },
-    //     placeholder: "Compose an epic...",
-    //     theme: "snow",
-    // });
-
-    // Swal.fire("SweetAlert2 is working!");
-});
+}
 
 //generate form with sweetalerts2
-async function modal({ title, html, form }) {
+async function modal({
+    title,
+    html,
+    formId,
+    url,
+    method,
+    callback = () => {},
+}) {
     const { value: formValues } = await Swal.fire({
         title,
         html,
         focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        showLoaderOnConfirm: true,
         preConfirm: () => {
-            return new FormData(document.getElementById("form"));
+            let form = document.getElementById(formId);
+            return {
+                url,
+                method,
+                formData: new FormData(form),
+            };
         },
+        allowOutsideClick: () => !Swal.isLoading(),
     });
+
     if (formValues) {
-        // let formData = new formData();
-        // formData.append('name', formValues[0]);
-        // formData.append('email', formValues[0]);
-        console.log(formValues);
-        // Swal.fire(JSON.stringify(formValues));
+        let { url, formData, method } = formValues;
+
+        try {
+            let data = await postData(url, formData, method);
+
+            notif("success", "Berhasil!", data.message);
+            callback();
+        } catch (error) {
+            notif("error", "Galat!", error);
+        }
     }
+}
+
+async function getRequestData(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (!data.status) {
+        throw new Error(data.message);
+    }
+
+    return data;
+}
+
+async function postData(url, formData, method) {
+    let response = await fetch(url, {
+        method,
+        body: formData,
+    });
+
+    let data = await response.json();
+
+    if (!data.status) {
+        throw new Error(data.message);
+    }
+
+    return data;
 }
