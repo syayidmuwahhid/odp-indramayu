7f588207d9a9c413c76dca38ead2caff95460521
syayid <muwahhidsyayid@gmail.com>
2024-06-15 16:56:14 +0700

    add relasi into model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'article_tag_id',
+    ];
+
+    public function ArticleTag(): BelongsTo
+    {
+        return $this->belongsTo(ArticleTag::class);
+    }
 }
