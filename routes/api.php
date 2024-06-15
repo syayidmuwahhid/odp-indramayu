157c5119743d7a430418b03e110d5aae714e89bd
syayid <muwahhidsyayid@gmail.com>
2024-06-15 17:04:37 +0700

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagsController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VideoController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +15,13 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('user', UserController::class)->except('update');
 Route::post('/user/{user}', [UserController::class, 'update'])->name('user.update');
+
+Route::apiResource('article', ArticleController::class);
+
+Route::apiResource('tags', TagsController::class)->except('update');
+Route::post('/tags/{tags}', [TagsController::class, 'update'])->name('tags.update');
+
+Route::apiResource('category', Category::class)->except('update');
+Route::post('/category/{category}', [CategoryController::class, 'update'])->name('category.update');
+
+Route::apiResource('video', VideoController::class);
