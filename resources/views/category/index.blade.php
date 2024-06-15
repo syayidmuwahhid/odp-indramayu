dbe998385674ddfd0c4c18488366669942ad4b3f
jannatunn <128748428+jannatunn@users.noreply.github.com>
2024-06-15 17:57:38 +0700

    add category

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Manajemen User')
+@section('title', 'category')
 
 @section('content')
     <div class="w-full px-6 py-6 mx-auto">
@@ -11,7 +11,7 @@
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div
                         class="p-6 pb-0 mb-0 flex justify-between bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>Tabel User</h6>
+                        <h6>Tabel Category</h6>
 
                         {{-- button modal --}}
                         <button id="btnAddModal" type="button" class="inline-block px-8 py-2 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer active:opacity-85 leading-pro text-xs ease-soft-in tracking-tight-soft bg-150 bg-x-25 hover:scale-102 active:shadow-soft-xs border-fuchsia-500 text-fuchsia-500 hover:text-fuchsia-500 hover:opacity-75 hover:shadow-none active:scale-100 active:border-fuchsia-500 active:bg-fuchsia-500 active:text-white hover:active:border-fuchsia-500 hover:active:bg-transparent hover:active:text-fuchsia-500 hover:active:opacity-75">Tambah</button>
@@ -27,14 +27,7 @@
 
                                         <th
                                             class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            Nama</th>
-                                        <th
-                                            class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            Email</th>
-
-                                        <th
-                                            class="px-6 py-3 font-semibold capitalize align-middle bg-transparent border-b border-gray-200 border-solid shadow-none tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                        </th>
+                                            Nama kategori</th>
                                     </tr>
                                 </thead>
                                 <tbody id="tbody_data">
@@ -53,5 +46,5 @@
 @endsection
 
 @push('js')
-<script src="{{ asset('assets/js/pages/user-index.js') }}"></script>
+<script src="{{ asset('assets/js/pages/category-index.js') }}"></script>
 @endpush
