76d53951d8f441c021e478f8ada8f890cc9a8da4
syayid <muwahhidsyayid@gmail.com>
2024-06-15 19:33:46 +0700

    update route slider

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,5 @@ Route::name('admin.')->prefix('admin')->middleware('auth')->group(function () {
     Route::get('/category', fn () => view('category.index'))->name('category.index');
     Route::get('/tag', fn () => view('tag.index'))->name('tag.index');
 
-    Route::get('/slider', function () {
-        return view('slider.index');
-    })->name('slider');
+    Route::get('/slider', fn () => view('slider.index'))->name('slider');
 });
