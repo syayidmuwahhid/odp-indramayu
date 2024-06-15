fc561d4706956937c019cd5213460f743005d676
jannatunn <128748428+jannatunn@users.noreply.github.com>
2024-06-15 10:01:53 +0700

    add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -118,6 +118,7 @@
                   </div>
                   <div class="flex-auto p-6">
                     <form role="form" method="POST" action="{{ route('login') }}">
+                        @csrf
                       <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Email</label>
                       <div class="mb-4">
                         <input required value="{{ old('email') }}" name="email" type="email" class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="Email" aria-label="Email" aria-describedby="email-addon" />
