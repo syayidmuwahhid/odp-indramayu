75062699c33f286b8ce6974f2489a81076963a63
syayid <muwahhidsyayid@gmail.com>
2024-06-20 15:54:26 +0700

    tag page

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -11,7 +11,7 @@
                     class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                     <div
                         class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                        <h6>Tabel tag</h6>
+                        <h6>Tabel Tag</h6>
 
                     </div>
                     <div class="flex-auto px-0 pt-0 pb-2">
