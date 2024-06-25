37291b38504df5d6dc0f51e60223382917a17747
syayid <muwahhidsyayid@gmail.com>
2024-06-25 10:49:10 +0700

    add contact page

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\SliderController;
 use App\Http\Controllers\TagsController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideoController;
+use App\Mail\ContactMail;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -16,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::apiResource('user', UserController::class);
-// Route::post('/user/{user}', [UserController::class, 'update'])->name('user.update');
 
 Route::apiResource('article', ArticleController::class);
 
@@ -31,3 +32,28 @@ Route::apiResource('category', CategoryController::class);
 Route::apiResource('video', VideoController::class);
 
 Route::apiResource('slider', SliderController::class);
+
+Route::post('/send-email', function (Request $request) {
+    $resp = [
+        'status' => false,
+    ];
+    $code = 500;
+
+    try {
+        $data = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'message' => $request->input('message'),
+        ];
+
+        Mail::to('muwahhidsyayid@gmail.com')->send(new ContactMail($data));
+
+        $resp['data'] = $request->all();
+        $resp['status'] = true;
+        $resp['message'] = 'Berhasil Mengirim Pesan!';
+        $code = 200;
+    } catch (\Throwable $th) {
+        $resp['message'] = $th->getMessage();
+    }
+    return response()->json($resp, $code);
+});
