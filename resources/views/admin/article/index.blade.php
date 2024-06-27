edf6013b409949abe84d1ad926adf16d9f5b304a
Syayidul Muwahhid <77384052+syayidmuwahhid@users.noreply.github.com>
2024-06-27 13:50:45 +0700

    Revert "edit list article"

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -18,8 +18,8 @@
                             class="inline-block px-8 py-2 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer active:opacity-85 leading-pro text-xs ease-soft-in tracking-tight-soft bg-150 bg-x-25 hover:scale-102 active:shadow-soft-xs border-green-500 text-green-500 hover:text-green-900 hover:opacity-75 hover:shadow-none active:scale-100 active:border-green-900 active:bg-green-900 active:text-white hover:active:border-green-900 hover:active:bg-transparent hover:active:text-green-900 hover:active:opacity-75">Tambah</a>
                     </div>
                     <div class="flex-auto px-0 pt-0 pb-2">
-                        <div class="px-10 overflow-auto" id="table_container">
-                            <table id="table_data" class=" items-center w-full mb-0 align-top border-gray-200 text-slate-500">
+                        <div class="px-10" id="table_container">
+                            <table id="table_data" class="table-fixed items-center w-full mb-0 align-top border-gray-200 text-slate-500">
                                 <thead class="align-bottom">
                                     <tr>
                                         <th
