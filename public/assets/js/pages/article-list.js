7e140a4838d9a622bc99dce26db353cec5e3059c
syayid <muwahhidsyayid@gmail.com>
2024-07-01 14:41:28 +0700

    fix logo user

--- public/assets/js/pages/article-list.js
+++ public/assets/js/pages/article-list.js
@@ -107,7 +107,7 @@ function setArticle() {
                 </span>`;
         html += `<span class="inline-flex items-center text-xs text-[#666]">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-1 1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7-6a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm-3 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm-7 0a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm-3 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                     </svg>
                     <p class="pl-1">${element.user_name}</p>
                 </span>`;
