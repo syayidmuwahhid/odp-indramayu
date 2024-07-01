d6b434d9a9815ab0b491ac7bb0a9e27cc50892fc
syayid <muwahhidsyayid@gmail.com>
2024-07-01 11:30:53 +0700

    button dociment

--- public/assets/js/pages/document.js
+++ public/assets/js/pages/document.js
@@ -27,7 +27,7 @@ async function getData() {
                             ${convertDate(element.date)}
                         </td>
                         <td class="py-3 px-6 text-center">
-                            <a href="${linkFile}" target="_blank" class="bg-blue-500 text-white px-4 py-2 rounded">Download</a>
+                            <a href="${linkFile}" target="_blank" class="text-white px-4 py-2 rounded" style="background-color: #da6220">Download</a>
                         </td>
                     </tr>`;
             }
